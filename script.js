1175cc4e9df2235d3ccfb928ec86314f4f225134
Digital Desk Academy <DigitalDeskAcademy@outlook.com>
2024-06-02 07:54:30 -0700

    Basic calculator GUI built

--- script.js
+++ script.js
@@ -6,5 +6,40 @@
 
 // Code goes here
 
+//Variables
+const int1 = 0;
+const int2 = 0
+const operator = ''
+
+// Selectors
+
+let display = document.querySelector('display');
+
+
+
+const operate = function (operator, int1, int2) {
+
+}
+
+
+
+
+// Button Selections
+const button1 = document.getElementById('1');
+
+
+// Mathmatical functions
+const add = function(a,b) {
+    return a + b;
+}
+const subtract = function(a,b) {
+    return a - b;
+}
+const multiply = function(a,b) {
+    return a * b;
+}
+const divide = function(a,b) {
+    return a / b;
+}
 
 
